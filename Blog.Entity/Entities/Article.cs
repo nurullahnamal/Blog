d839a3399daff2bb1bc5d91ccc5d92c00a44b226
nurullahnamal <nurullahnamal4@gmail.com>
2024-11-14 17:09:57 +0300

    sql connection

--- Blog.Entity/Entities/Article.cs
+++ Blog.Entity/Entities/Article.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +7,17 @@ using System.Threading.Tasks;
 
 namespace Blog.Entity.Entities
 {
-	public class Article
+	public class Article:EntityBase
 	{
-		public Guid Id { get; set; }
 		public string Title { get; set; }
 		public string Content{ get; set; }
 		public int ViewCount { get; set; }
 
 		public Guid CategoryId { get; set; }
 		public Category Category{ get; set; }
+
 		public Guid ImageId { get; set; }
 		public Image  Image{ get; set; }
+	
 	}
 }
